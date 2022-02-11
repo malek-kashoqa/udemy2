9736cf1a4ce587a51da097d0337066943d55d485
malek-kashoqa <201720463@std-zuj.edu.jo>
2022-02-11 17:36:04 +0200

    Finish Login Screen lesson

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -8,6 +8,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  bool isPasswordVisible = false;
+  Icon passwordVisibleIcon = Icon(Icons.remove_red_eye);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,28 +21,106 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Login",
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  onFieldSubmitted: (value) {
+                    print(value);
+                  },
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Email Address',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: !isPasswordVisible,
+                  onFieldSubmitted: (value) {
+                    print(value);
+                  },
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: passwordVisibleIcon,
+                      onPressed: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                          // if (isPasswordVisible) {
+                          //   passwordVisibleIcon = Icon(Icons.visibility_off);
+                          // } else {
+                          //   passwordVisibleIcon = Icon(Icons.visibility);
+                          // }
+                          isPasswordVisible
+                              ? passwordVisibleIcon = Icon(Icons.visibility_off)
+                              : passwordVisibleIcon = Icon(Icons.visibility);
+                          ;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: MaterialButton(
+                    onPressed: () {
+                      print(emailController.text);
+                      print(passwordController.text);
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    color: Colors.blue,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don\'t have an account?"),
+                    TextButton(onPressed: () {}, child: Text("Register Now"))
+                  ],
+                )
+              ],
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: 'Email Address',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.email),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
